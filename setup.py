d5cf06100b32c95c275337d658fac634fa5d9337
aamir490 <aamirimran490@gmail.com>
2025-09-01 11:04:45 +0000

    1st commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ml pipeline with dvc vc',
+    author='campusx',
+    license='MIT',
+)
